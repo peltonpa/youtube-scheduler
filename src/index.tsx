df1695e50f918617b4f15f7bd80ce6380c0e6cdf
Paavo Peltonen <paavo@sharkpunch.com>
2023-10-01 21:12:23 +0300

    New fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import '@fontsource/raleway/400.css'
+import '@fontsource/open-sans/700.css'
 import './index.css';
 import App from './App';
 
+const theme = extendTheme({
+  fonts: {
+    heading: `'Open Sans', sans-serif`,
+    body: `'Raleway', sans-serif`,
+  },
+})
+
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
